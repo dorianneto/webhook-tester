3df39cb663ac71f186bf3e88c83818f9842e897f
Dorian Neto <doriansampaioneto@gmail.com>
2020-05-20 15:28:38 -0700

    add polling as fallback transport to websocket

--- src/services/websocket.js
+++ src/services/websocket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 const socket = io(process.env.REACT_APP_WEBSOCKET_SERVER_URL, {
   autoConnect: false,
-  transports: ['websocket']
+  transports: ['websocket', 'polling']
 });
 
 const connect = (token) => {
